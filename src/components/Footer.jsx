b6967c975b37dce4fd16c69fffd3ee918cb95138
Kijeosowo <pelumiwilliams23@gmail.com>
2025-11-27 00:07:17 +0100

    Initialized the repo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Footer = () => {
+  return;
+  <footer>
+    <section>
+      <div></div>
+
+      <div>
+        <ul>
+            <li><a href="#">Get started</a></li>
+            <li><a href="#">Download app</a></li>
+            <li><a href="#">New releases</a></li>
+            <li><a href="#">Originals</a></li>
+            <li><a href="#">Plans</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Account</a></li>
+            <li><a href="#">Sign in</a></li>
+            <li><a href="#">Platform</a></li>
+            <li><a href="#">Support</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Pride</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">New releases</a></li>
+            <li><a href="#">Originals</a></li>
+            <li><a href="#">Plans</a></li>
+        </ul>
+      </div>
+
+      <div>
+        <button>Sign up</button>
+      </div>
+    </section>
+  </footer>;
+};
+
+export default Footer;
