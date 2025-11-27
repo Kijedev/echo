833cbd7f069ccb9bd253e05dd158cd9135128a98
Kijeosowo <pelumiwilliams23@gmail.com>
2025-11-27 01:19:09 +0100

    added the footer-card to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,18 @@ import { FaYoutube } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { AiFillTikTok } from "react-icons/ai";
 import { FaFacebookSquare } from "react-icons/fa";
+import FooterCard from "../components/FooterCard.jsx";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0A0C10] pt-20 pb-10">
-      <section className="flex justify-around text-white">
+    <footer className="bg-[#0A0C10] pt-20 pb-10 px-5">
+      <FooterCard />
+      <section className="flex flex-col lg:flex-row justify-around text-white mt-20">
         <div>
           <img src={Logo} alt="Logo" />
         </div>
 
-        <div className="flex justify-around gap-20">
+        <div className="flex flex-col lg:flex-row justify-around gap-20">
           <ul className="text-[#D8D8D8] text-sm flex flex-col gap-3">
             <p className="text-[#7D7E80] mb-4">Get started</p>
             <li>
@@ -59,13 +61,15 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div>
-          <button className="bg-[#A41045] rounded-md text-white px-4 py-2">Sign up</button>
+        <div className="lg:mt-0 mt-10">
+          <button className="bg-[#A41045] rounded-md text-white px-4 py-2">
+            Sign up
+          </button>
         </div>
       </section>
 
       <section className="mt-20">
-        <div className="flex justify-center gap-20 items-center py-8">
+        <div className="flex flex-col lg:flex-row justify-center lg:gap-20 gap-10 items-center py-8">
           <p className="text-sm text-[#7D7E80]">
             Say goodbye to interruptions and enjoy uninterrupted music
             streaming.
@@ -80,7 +84,7 @@ const Footer = () => {
 
         <hr className="mx-auto max-w-3xl text-[#7D7E80]" />
 
-        <div className="flex justify-center gap-10 py-8 text-[#7D7E80] text-sm">
+        <div className="flex flex-col lg:flex-row justify-center gap-10 py-8 text-[#7D7E80] text-sm">
           <p>2025 echo</p>
           <p>Cookie settings</p>
           <p>Terms and conditions</p>
